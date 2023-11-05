b58f419083c4a1dc9cc06b80839235be5c0402a7
Light Yagami <145213143+071624@users.noreply.github.com>
2023-11-06 04:27:13 +0530

    Add files via upload

---
+++ user_defined_package/Test.java
@@ -0,0 +1,7 @@
+package user_defined_package;
+
+public class Test {
+    public void show(){
+        System.out.println("Hello world");
+    }
+}
---
+++ user_defined_package/TestMain.java
@@ -0,0 +1,8 @@
+import user_defined_package.Test;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Test obj = new Test();
+        obj.show();
+    }
+}
